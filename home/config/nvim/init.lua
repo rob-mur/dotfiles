c075696343beaf67f7150ab9d8e8e6160578f7ba
Rob Murphy <rmurphyswimmer@gmail.com>
2026-01-06 09:58:53 +0100

    fix: duplicate basedpyright

--- home/config/nvim/init.lua
+++ home/config/nvim/init.lua
@@ -801,10 +801,12 @@ require("lazy").setup({
 
 			-- Non mason managed servers (nix issues)
 			vim.lsp.config("basedpyright", {
+				cmd = { "basedpyright-langserver", "--stdio" },
+				filetypes = { "python" },
+				root_markers = { "pyrightconfig.json", "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
 				settings = {
 					basedpyright = {
 						analysis = {
-							-- This remains the key to getting project-wide diagnostics
 							diagnosticMode = "workspace",
 							autoSearchPaths = true,
 							useLibraryCodeForTypes = true,
