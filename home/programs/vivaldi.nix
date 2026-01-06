b662694d74fe0022f1ea48cd08d8f38a3d30b56b
Rob Murphy <rmurphyswimmer@gmail.com>
2026-01-06 09:58:53 +0100

    fix: white background vivaldi

--- home/programs/vivaldi.nix
+++ home/programs/vivaldi.nix
@@ -1,4 +1,9 @@
-{config, ...}: let
+{config, pkgs, ...}: let
 in {
-  programs.vivaldi.enable = true;
+  programs.vivaldi = {
+    enable = true;
+    package = pkgs.vivaldi.override {
+      commandLineArgs = ["--ozone-platform=x11"];
+    };
+  };
 }
