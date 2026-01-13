4d8ed7b3d983840b32b8edd6cc2dc66a29ab6497
Rob Murphy <robert.murphy@descartesunderwriting.com>
2026-01-13 11:58:59 +0100

    chore: temp disable duet

--- hosts/work-laptop/work-laptop.nix
+++ hosts/work-laptop/work-laptop.nix
@@ -57,7 +57,7 @@ in {
   # Specific env vars just for work
   home.sessionVariables = {
     UV_KEYRING_PROVIDER = "subprocess";
-    GOOGLE_CLOUD_PROJECT = "duet-assistant";
+    # GOOGLE_CLOUD_PROJECT = "duet-assistant";
     GITLAB_API_TOKEN = "$(cat $HOME/.auth/gitlab_token)";
     GOOGLE_APPLICATION_CREDENTIALS = "$HOME/.config/gcloud/application_default_credentials.json";
   };
