6df8660056ba75cdbb3773758d27ebbe31188ab9
Rob Murphy <robert.murphy@descartesunderwriting.com>
2026-03-23 11:48:02 +0100

    fix: kde connect

--- hosts/work-laptop/flake.nix
+++ hosts/work-laptop/flake.nix
@@ -19,7 +19,35 @@
     home-manager,
     nixgl,
     ...
-  }: {
+  }: let
+    overlay-gtk-portal = final: prev: {
+      xdg-desktop-portal-gtk = prev.xdg-desktop-portal-gtk.overrideAttrs (oldAttrs: {
+        postInstall =
+          (oldAttrs.postInstall or "")
+          + ''
+            # Fix UseIn to include sway, not just gnome
+            sed -i 's/UseIn=gnome/UseIn=gnome;sway/' $out/share/xdg-desktop-portal/portals/gtk.portal
+          '';
+      });
+    };
+    overlay-hypr-remote = final: prev: {
+      xdg-desktop-portal-hypr-remote = prev.stdenv.mkDerivation {
+        pname = "xdg-desktop-portal-hypr-remote";
+        version = "unstable-2025-06-09";
+        src = prev.fetchFromGitHub {
+          owner = "gac3k";
+          repo = "xdg-desktop-portal-hypr-remote";
+          rev = "f463018129c5effd3b82b477d7f84fe0d0820a6b";
+          hash = "sha256-hbRlPcrPWOKWZvLlnsw37/s4P+bLRq59n+R9qtVbIXc=";
+        };
+        patches = [../../nixos/patches/hypr-remote-keysym.patch];
+        patchFlags = ["--ignore-whitespace" "-p1" "-F3"];
+        nativeBuildInputs = with prev; [cmake pkg-config wayland-scanner];
+        buildInputs = with prev; [wayland.dev wayland-protocols libei sdbus-cpp libffi systemd libxkbcommon];
+        cmakeFlags = ["-DCMAKE_INSTALL_LIBEXECDIR=bin" "-DCMAKE_EXE_LINKER_FLAGS=-lxkbcommon"];
+      };
+    };
+  in {
     homeConfigurations."dev" = home-manager.lib.homeManagerConfiguration {
       pkgs = import nixpkgs {
         system = "x86_64-linux";
@@ -28,7 +56,12 @@
           (import ./overlays.nix {inherit nixgl;})
         ];
       };
-      modules = [./work-laptop.nix];
+      modules = [
+        ({...}: {
+          nixpkgs.overlays = [overlay-gtk-portal overlay-hypr-remote];
+        })
+        ./work-laptop.nix
+      ];
     };
 
     # Expose home-manager for easy running
