5ebb99385f34e203959d90e14f974688b865472e
Rob Murphy <rmurphyswimmer@gmail.com>
2026-04-06 19:40:44 +0200

    fix: kanshi

--- home/services/kanshi.nix
+++ home/services/kanshi.nix
@@ -124,15 +124,15 @@ in {
           ];
           profile.exec = [
             # Create and move workspaces in reverse order, alternating
-            "swaymsg workspace 10, move workspace to output DP-2"
+            "swaymsg workspace 10, move workspace to output DP-3"
             "swaymsg workspace 9, move workspace to output DP-4"
-            "swaymsg workspace 8, move workspace to output DP-2"
+            "swaymsg workspace 8, move workspace to output DP-3"
             "swaymsg workspace 7, move workspace to output DP-4"
-            "swaymsg workspace 6, move workspace to output DP-2"
+            "swaymsg workspace 6, move workspace to output DP-3"
             "swaymsg workspace 5, move workspace to output DP-4"
-            "swaymsg workspace 4, move workspace to output DP-2"
+            "swaymsg workspace 4, move workspace to output DP-3"
             "swaymsg workspace 3, move workspace to output DP-4"
-            "swaymsg workspace 2, move workspace to output DP-2"
+            "swaymsg workspace 2, move workspace to output DP-3"
             "swaymsg workspace 1, move workspace to output DP-4"
           ];
         }
