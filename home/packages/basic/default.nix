7e6ea8840c3233ac1972b32daec2b144b0962989
Rob Murphy <rmurphyswimmer@gmail.com>
2026-04-14 15:19:24 +0200

    feat: global node for claude plugins

--- home/packages/basic/default.nix
+++ home/packages/basic/default.nix
@@ -1,3 +1,3 @@
 {...}: {
-  imports = [./openssl.nix ./archive.nix ./media.nix ./utility.nix ./wayland.nix];
+  imports = [./node.nix ./openssl.nix ./archive.nix ./media.nix ./utility.nix ./wayland.nix];
 }
